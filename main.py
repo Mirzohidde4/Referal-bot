654e9ff52d1b79ca3e8c45bb49e5c92c39acfdd3
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-08-07 19:28:04 +0500

    tel raqam

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio, logging
+from aiogram import Bot, Dispatcher, F, html
+from aiogram.types import (
+    Message,
+    CallbackQuery,
+    FSInputFile,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    KeyboardButton,
+    ReplyKeyboardMarkup
+)
+from aiogram.filters import CommandStart, Command, and_f
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.context import FSMContext
+from config import TOKEN
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def smd_start(message: Message):
+    await message.reply(
+        text=f"{html.bold("Assalomu Alaykum xush kelibsiz.\nbotdan foydalanish uchun telefon raqamingizni yuboring.")}",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Telefon raqam yuborish", request_contact=True)]
+            ], 
+            resize_keyboard=True, one_time_keyboard=True,
+            input_field_placeholder="telefon raqamingizni yuboring"
+        )
+    )
+
+
+@dp.message(F.contact)
+async def telefon(message: Message):
+    tel = message.c
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except:
+        print("bot o`chdi")
